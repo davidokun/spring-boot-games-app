0651dc3fc588e585e12b118a8468b225c1268c9b
David Marin <davidokun99@gmail.com>
2017-06-04 21:33:50 -0500

    - Initial project setup

---
+++ app/src/main/java/com/singletonapps/SpringBootGamesAppApplication.java
@@ -0,0 +1,12 @@
+package com.singletonapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGamesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGamesAppApplication.class, args);
+	}
+}
